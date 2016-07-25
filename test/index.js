192c3fee4362d7f86c716e549014385440ffb464
zhouyg <yunge@taovip.com>
2016-07-25 14:45:30 +0800

    add fn for resize image data

--- test/index.js
+++ test/index.js
@@ -1,16 +1,18 @@
 var body = document.querySelector('body')
 
-
-var m = pixiLib.utils.mpLoading()
-
-body.appendChild(m.el())
+var arr = pixiLib.utils.repeat('#000',3).concat(['#fff','#fff'])
+  .concat(pixiLib.utils.repeat('#000',3)).concat(pixiLib.utils.repeat('#fff',20))
 
 var i = 0;
+var r = 1;
 
+console.log(arr.length)
+arr = pixiLib.utils.resizeImageData(arr,6,r)
+console.log(arr.length)
 
-setInterval(function(){
+var m = pixiLib.utils.matrixManager(arr)
 
-  m.progress(i++)
-
-},100)
-window.m = m;
+body.appendChild(m.el({
+  width:(72*r)+'px',
+  background:'#999'
+}))
