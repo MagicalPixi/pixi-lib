6761bc4f9360e834ecd5e9bf046652f0ffdb7011
zhou-yg <zhou.y.g890@gmail.com>
2016-07-27 00:37:54 +0800

    fix resizeImageData func

--- test/index.js
+++ test/index.js
@@ -1,18 +1,40 @@
-var body = document.querySelector('body')
+var c = document.querySelector('#c')
 
-var arr = pixiLib.utils.repeat('#000',3).concat(['#fff','#fff'])
-  .concat(pixiLib.utils.repeat('#000',3)).concat(pixiLib.utils.repeat('#fff',20))
+var context = c.getContext('2d')
 
-var i = 0;
-var r = 1;
+var img = new Image
 
-console.log(arr.length)
-arr = pixiLib.utils.resizeImageData(arr,6,r)
-console.log(arr.length)
+img.src = '/test/test.png'
 
-var m = pixiLib.utils.matrixManager(arr)
+img.onload = function(){
 
-body.appendChild(m.el({
-  width:(72*r)+'px',
-  background:'#999'
-}))
+  c.width = img.width
+  c.height = img.height
+
+  context.drawImage(img,0,0)
+
+  re()
+}
+
+
+window.re = function  re() {
+
+  var imd = context.getImageData(0,0,c.width,c.height)
+
+  console.log(imd.data.length,imd.data)
+
+  var d = pixiLib.utils.resizeImageData(imd.data,c.width * 4,2,2,4)
+
+  console.log(d.length,d)
+
+  var imd2 = new ImageData(c.width * 2,c.height * 2)
+
+  d.forEach((v,i)=>{
+    imd2.data[i] = v
+  })
+
+  c.width = c.width * 2
+  c.height = c.height * 2
+
+  context.putImageData(imd2,0,0)
+}
